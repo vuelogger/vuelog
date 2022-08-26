17f4d1b13904e3fb60962e29618cc8f4971a9228
bwealthy72 <bwealthy72@gmail.com>
2022-08-26 22:35:25 +0900

    카테고리에서는 head 읽지 않도록 수정

--- pages/post/_category/_id.vue
+++ pages/post/_category/_id.vue
@@ -5,62 +5,59 @@
 <script>
 export default {
   head() {
-    return {
-      meta: [
-        { hid: "test", property: "test", content: "Test Title" },
-        {
-          hid: "twitter:title",
-          name: "twitter:title",
-          content: this.post.title,
-        },
-        {
-          hid: "og:title",
-          name: "og:title",
-          content: this.post.title,
-        },
-        {
-          hid: "og:image",
-          property: "og:image",
-          content: this.post.cover,
-        },
-        {
-          hid: "twitter:image",
-          name: "twitter:image",
-          content: this.post.cover,
-        },
-        {
-          hid: "og:url",
-          property: "og:url",
-          content: this.url,
-        },
-        {
-          hid: "description",
-          name: "description",
-          content: this.post.description,
-        },
-        {
-          hid: "og:description",
-          property: "og:description",
-          content: this.post.description,
-        },
-        {
-          hid: "twitter:description",
-          name: "twitter:description",
-          content: this.post.description,
-        },
-        {
-          hid: "twitter:card",
-          name: "twitter:card",
-          content: "summary_large_image",
-        },
-      ],
-    };
+    if (this.post) {
+      return {
+        meta: [
+          {
+            hid: "twitter:title",
+            name: "twitter:title",
+            content: this.post.title,
+          },
+          {
+            hid: "og:title",
+            name: "og:title",
+            content: this.post.title,
+          },
+          {
+            hid: "og:image",
+            property: "og:image",
+            content: this.post.cover,
+          },
+          {
+            hid: "twitter:image",
+            name: "twitter:image",
+            content: this.post.cover,
+          },
+          {
+            hid: "og:url",
+            property: "og:url",
+            content: this.url,
+          },
+          {
+            hid: "description",
+            name: "description",
+            content: this.post.description,
+          },
+          {
+            hid: "og:description",
+            property: "og:description",
+            content: this.post.description,
+          },
+          {
+            hid: "twitter:description",
+            name: "twitter:description",
+            content: this.post.description,
+          },
+          {
+            hid: "twitter:card",
+            name: "twitter:card",
+            content: "summary_large_image",
+          },
+        ],
+      };
+    }
   },
-  data() {
-    return {
-      url: "",
-    };
-  },
+
   async asyncData({ route, store }) {
     const params = route.params || {};
     let category = params ? params.category : "";
